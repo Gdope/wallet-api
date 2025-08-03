308980f6661088fb646cdfa027c8e7edf9296036
Gdope <snedeljkovic2003@gmail.com>
2025-08-03 20:15:16 +0200

    Fix sql import

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,7 @@
 import { neon } from "@neondatabase/serverless";
-
 import "dotenv/config";
 
-// Creates a SQL connection using our DB URL
-const sql = neon(
-  "postgresql://neondb_owner:npg_P9wJrIZjl7Ms@ep-aged-boat-ad6no56o-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-);
+export const sql = neon(process.env.DATABASE_URL);
 
 export async function initDB() {
   try {
@@ -21,6 +17,6 @@ export async function initDB() {
     console.log("Database initialized successfully");
   } catch (error) {
     console.log("Error initializing DB", error);
-    process.exit(1); // status code 1 means failure, 0 success
+    process.exit(1);
   }
 }
